c8e3312bea1700a58bdf4fa9001afe82bc004f61
Andres Felipe <afelipe.ospina@gmail.com>
2024-04-28 22:59:50 -0500

    Modelos categoria y producto

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SupermarketContext : DbContext
+    {
+        protected SupermarketContext(DbContextOptions options) : base (options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        
+        
+    }
+}
