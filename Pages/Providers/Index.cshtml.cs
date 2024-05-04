58201102cebce93fc06fae84176d5a8d46e1de95
Andres Felipe <afelipe.ospina@gmail.com>
2024-05-04 04:43:59 -0500

    Index Providers

---
+++ Pages/Providers/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Provider> Providers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Providers = await _context.Providers.ToListAsync();
+        }
+    }
+}
