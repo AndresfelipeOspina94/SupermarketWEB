cd1c97e0ca4b28ac2fc8a85c0a1e1b8b7ae78a9d
Andres Felipe <afelipe.ospina@gmail.com>
2024-05-04 04:26:55 -0500

    Create PayMode

---
+++ Pages/PayModes/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Threading.Tasks;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = new PayMode();
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || PayMode == null)
+            {
+                return Page();
+            }
+
+            _context.PayModes.Add(PayMode);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
