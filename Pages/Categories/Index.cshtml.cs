87a11edb28401bbea997dd8a3c6a05d714f93777
Andres Felipe <afelipe.ospina@gmail.com>
2024-05-04 00:22:17 -0500

    Index categories

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        public IList<Category> Categories { get; set; } = new List<Category>();
+        public async Task OnGetAsync()
+        {
+            Categories = await _context.Categories.ToListAsync();
+        }
+    }
+}
